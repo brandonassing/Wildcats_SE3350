9e6fed2562afc067e6e1d41dbc0035463901c16a
Kristian Strong <kristianstrong5@gmail.com>
2017-02-23 00:25:01 -0500

    added delete components to the awards section also, delete works

--- FrontEnd/studentsRecords/app/components/delete-standing.js
+++ FrontEnd/studentsRecords/app/components/delete-standing.js
@@ -4,11 +4,8 @@ export default Ember.Component.extend({
 
     store:Ember.inject.service(),
     
-    isEditing:false,
-
     actions: {
         deleteCourse (thisCourse){
-            this.set('isEditing',false);
             thisCourse.set('student', null);
             thisCourse.save();
             thisCourse.destroyRecord();
