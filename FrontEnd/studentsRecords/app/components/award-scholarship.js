4bff95c5d778c9f35fd453aeeb62a856ff32439a
brandonassing <bassing@uwo.ca>
2017-03-01 17:29:21 -0500

    added delete award modal

--- FrontEnd/studentsRecords/app/components/award-scholarship.js
+++ FrontEnd/studentsRecords/app/components/award-scholarship.js
@@ -1,54 +1,62 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    store:Ember.inject.service(),
-    note: null,
-    student: null,
-    AwardModel: null,
-    currentStudent:null,
-    isEditing:false,
-    
-    init() {
+  store: Ember.inject.service(),
+  note: null,
+  student: null,
+  AwardModel: null,
+  currentStudent: null,
+  isEditing: false,
+  deleteModalShowing: false,
+
+  init() {
     this._super(...arguments);
     var self = this;
     this.get('store').findAll('award').then(function (records) {
       self.set('awardsModel', records);
     });
-    },
+  },
 
-    actions: {
-        addAward (){
+  actions: {
+    addAward() {
       this.get("store").createRecord('award', {
         "note": this.get("note"),
         "student": this.get("currentStudent"),
-        
-        }).save().then(() => {
-            this.set("note",null);
-        });
-    },
-        deleteAward(thisAward){
-            if(confirm('Are you sure that you want to delete this file? \n This cannot be undone.')){
-                thisAward.set('student',null);
-                thisAward.save();
-                thisAward.destroyRecord();
-            }else{
-                return;
-            }            
-        },
-        editAward(thisAward){
-            this.set('isEditing',true);
-        },
-        saveAward(thisAward){
-            if(confirm("Are you sure you want to save? \n All previous information will be lost.")){
-                thisAward.save();
-                this.set('isEditing',false);
-            }else{
-                return;
-            }
 
-        },
-        cancelEdit(){
-        this.set('isEditing',false);
-        }
+      }).save().then(() => {
+        this.set("note", null);
+      });
+    },
+    toggleDeleteModal() {
+      if (this.get("deleteModalShowing")) {
+        $('#delete-modal-award')
+          .modal('hide');
+        this.set("deleteModalShowing", false);
+      } else {
+        $('#delete-modal-award')
+          .modal('show');
+        this.set("deleteModalShowing", true);
+      }
+    },
+    deleteAward(thisAward) {
+      thisAward.set('student', null);
+      thisAward.save();
+      thisAward.destroyRecord();
+    },
+    editAward(thisAward) {
+      this.set('isEditing', true);
+    },
+    saveAward(thisAward) {
+      if (confirm("Are you sure you want to save? \n All previous information will be lost.")) {
+        thisAward.save();
+        this.set('isEditing', false);
+      } else {
+        return;
+      }
+
+    },
+    cancelEdit() {
+      this.set('isEditing', false);
     }
+  }
 });
