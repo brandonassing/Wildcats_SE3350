800b8df6c8fdd93202aa659f4837fe07c7a71486
brandonassing <bassing@uwo.ca>
2017-03-13 23:22:12 -0400

    decent working auth

---
+++ BackEnd/studentsRecords/models/userRoles.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Abdelkader on 2017-02-23.
+ */
+var mongoose = require('mongoose');
+var userRoleSchema = mongoose.Schema({
+    dateAssigned: Date,
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: ('Users')
+    },
+    role: {
+        type: mongoose.Schema.ObjectId,
+        ref: ('RoleCode')
+    }
+});
+
+//var UserRoles = mongoose.model('student', userRoleSchema); ERROR TODO TODO WHY WAS IT ORIGINALLY THIS?????????????????????????????
+
+var UserRoles = mongoose.model('userRole', userRoleSchema);
+exports.Model = UserRoles;
