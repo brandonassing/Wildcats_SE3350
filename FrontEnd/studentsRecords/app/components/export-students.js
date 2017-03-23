e3d2916ec9225d0a5d4d7e6622cc9ff5b5e23081
spencerconnelly <sconnel6@uwo.ca>
2017-03-22 21:51:12 -0400

    good luck brandon

---
+++ FrontEnd/studentsRecords/app/components/export-students.js
@@ -0,0 +1,63 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    exportGender: function(){
+        var data = myStore.findAll('rolePermission');
+        console.log(data);
+            
+    },
+    exportResidency: function(){
+
+    },
+    exportStudents: function(){
+
+    },
+    exportAdvancedStandings: function(){
+
+    },
+    exportHighSchools: function(){
+
+    },
+    exportAwards: function(){
+
+    },
+    exportHighSchoolCourseInfo: function(){
+
+    },
+    exportUndergradCourses: function(){
+
+    },
+    exportUndergradRecordPlans: function(){
+
+    },
+    exportTermCodes: function(){
+
+    },
+    exportAdmissionComments: function(){
+
+    },
+    exportRegistartionComments: function(){
+
+    },
+    exportBasis: function(){
+
+    },
+    exportGender: function(){
+
+    },
+    exportGender: function(){
+
+    },
+    exportGender: function(){
+
+    },
+    exportGender: function(){
+
+    },
+    exportGender: function(){
+
+    },
+    exportGender: function(){
+
+    },
+});
