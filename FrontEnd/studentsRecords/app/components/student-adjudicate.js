a06bb094387d1e1e4dd15ebcfedadbe3fbc886fc
Kristian Strong <kristianstrong5@gmail.com>
2017-04-03 19:39:01 -0400

    got manual add adjudication codes started/kinda working

--- FrontEnd/studentsRecords/app/components/student-adjudicate.js
+++ FrontEnd/studentsRecords/app/components/student-adjudicate.js
@@ -1,5 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    isEditing:false,
+    store: Ember.inject.service(),
+    addModalShowing: false,
+    assessmentModel: null,
+
+    didRender() {
+        Ember.$('.menu .item').tab();
+    },
+    init() {
+        this._super(...arguments);
+        var self = this;
+        this.get('store').findAll('assessment-code').then(function (records) {
+            self.set('assessmentModel', records);
+        });
+    },
+    actions: {
+        addThisCode() {
+                this.get("store").createRecord('adjudication', {
+                "student": this.get("currentStudent"),
+                "assessment-code": this.get("thisCode")
+            });            
+        },
+        removeCode(thisOne){
+            thisOne.set('student',null);
+            thisOne.set('assessment-code',null);
+            thisOne.destroyRecord();
+            this.get("currentStudent").save();
+        },
+        toggleAddModal() {
+            if (this.get("addModalShowing")) {
+                $('#add-modal-adj')
+                    .modal('hide');
+                this.set("addModalShowing", false);
+            } else {
+                $('#add-modal-adj')
+                    .modal('show');
+                this.set("addModalShowing", true);
+            }
+        },
+    }
 });
