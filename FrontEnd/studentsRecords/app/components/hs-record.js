b97b7377cbf42d6ccfb2c67bbe58c4559199eaac
Kristian Strong <kristianstrong5@gmail.com>
2017-04-06 01:02:01 -0400

    got rid of the highschool tab, incase i cant get it to work by presentation

--- FrontEnd/studentsRecords/app/components/hs-record.js
+++ FrontEnd/studentsRecords/app/components/hs-record.js
@@ -2,13 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     store: Ember.inject.service(),
+    currentStudent: null,
     sSModel: null,
     subjectModel: null,
     courseModel: null,
     courseGradeModel: null,
-
-
-
 
 
     didRender() {
@@ -17,17 +15,17 @@ export default Ember.Component.extend({
     init() {
         this._super(...arguments);
         var self = this;
-        this.get("store").findAll("secondary-school").then(function (records) {
-            self.set("sSModel", records);
+        this.get('store').findAll('secondary-school').then(function (records) {
+            self.set('sSModel', records);
         });
-        this.get("store").findAll("high-school-subject").then(function (records) {
-            self.set("subjectModel", records);
+        this.get("store").findAll('high-school-subject').then(function (records) {
+            self.set('subjectModel', records);
         });
-        this.get("store").findAll("high-school-course").then(function (records) {
-            self.set("courseModel", records);
+        this.get("store").findAll('high-school-course').then(function (records) {
+            self.set('courseModel', records);
         });
-        this.get("store").findAll("hs-course-grade").then(function (records) {
-            self.set("courseGradeModel", records);
+        this.get('store').findAll('hs-course-grade').then(function (records) {
+            self.set('courseGradeModel', records);
         });
     },
 
